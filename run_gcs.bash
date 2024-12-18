a2116c0c9dd1f1ceee5e83ded6507e243a0adad7
Saurav Agarwal <agr.saurav1@gmail.com>
2024-12-18 09:42:08 -0500

    add offboard and gcs scripts

---
+++ run_gcs.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Enable error handling
+set -e
+
+# Function to show usage
+usage() {
+  echo "Usage: $0 [--create|--build|--bash|--origin|--pac|--rviz|--lpac]"
+  exit 1
+}
+
+# Check if any argument is provided
+if [ $# -eq 0 ]; then
+  usage
+fi
+
+# Parse the provided flags
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --create)
+      echo "Creating PAC container..."
+      bash ${PAC_WS}/pac_ws_setup/pac_create_container.sh -d "${PAC_WS}" --ns gcs -n gcs --noble
+      ;;
+    --build)
+      echo "Running build script..."
+      docker exec -it gcs bash -ci pac_ws_setup/gcs_build.bash
+      ;;
+    --bash)
+      echo "Starting bash in Docker container..."
+      docker exec -it gcs bash
+      ;;
+    --origin)
+      echo "Launching GCS origin..."
+      docker exec -it gcs bash -ci "ros2 launch launch/gcs_origin.yaml"
+      ;;
+    --pac)
+      echo "Running PAC status script..."
+      docker exec -it gcs bash -ci "ros2 run gcs status_pac"
+      ;;
+    --rviz)
+      echo "Launching RViz..."
+      xhost +
+      docker exec -it gcs bash -ci "export DISPLAY=:0; ros2 launch launch/rviz.yaml"
+      ;;
+    --lpac)
+      echo "Running LPAC status script..."
+      docker exec -it gcs bash -ci "ros2 launch launch/lpac.yaml"
+      ;;
+    *)
+      echo "Error: Invalid option '$1'"
+      usage
+      ;;
+  esac
+  shift
+done
+
